ff24e183afaf6c3c2bea357bc23c2faf4d486178
ha0ei <11146008@ntub.edu.tw>
2025-03-22 09:55:53 +0800

    add

---
+++ Frontend/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
+
+
+export default function App() {
+  return (
+    <View className='w-full h-full justify-center items-center '>
+      <Text className='text-red-500'>Open up App.js to start working on your app!</Text>
+      <Text classNasme='text-red-500'>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
